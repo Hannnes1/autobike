6a0bb407bdf6d4801403f4d613eb5b87874255df
Hannes Hultergård <hannes.hultergard@gmail.com>
2022-06-09 15:56:33 +0200

    Comment and improve matlab code

--- plotData.m
+++ plotData.m
@@ -1,20 +1,28 @@
-data = tdmsread("log5P3S45.tdms");
+% Read all of the data in the TDMS file.
+data = tdmsread("Logs/Test4.tdms");
 
 %% Encoder
+
+% `data` contains one cell per group. Each cell contains a table with one
+% column per channel. In this case, the first channel in the first group
+% contains the time, and the second channel contains the position etc.
 time1 = table2array(data{1,1}(:,1));
 position = table2array(data{1,1}(:,2));
 velocity = table2array(data{1,1}(:,3));
 
 figure;
+yyaxis left;
+% 611 is the conversion ratio from counts on the encoder to degrees.
 plot(time1-time1(1),position/611);
-ylim([-90,90]);
+ylim([-20,20]);
 ylabel("Position (degrees)");
 hold on;
 yyaxis right;
-plot(time1-time1(1),velocity/611);
-ylim([-1,1]);
-ylabel("Velocity (degrees/ms)");
+plot(time1-time1(1),velocity/611*1000);
+ylim([-50,50]);
+ylabel("Velocity (degrees/s)");
 xlabel("Time (seconds)");
+xlim([7,11.5]);
 title("Steering motor readings");
 hold off;
 
@@ -52,8 +60,9 @@ yyaxis right;
 plot(time2-time2(1),rollRate);
 ylabel("Roll rate");
 ylim([-10,10]);
+xlim([7,11.5]);
 xlabel("Time (seconds)");
-title("Roll rate compared to PWM");
+title("Duty cycle and Roll rate");
 
 
 
