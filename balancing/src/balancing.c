1327686c96e63e494c4e9c9f7ef7b3409d91feda
Hannes Hultergård <hannes.hultergard@gmail.com>
2022-06-14 15:08:02 +0200

    Fix PWM conversion

--- balancing/src/balancing.c
+++ balancing/src/balancing.c
@@ -9,9 +9,16 @@ extern double stabilizeBike(double rollRate, double Kp, double Ki, double Kd) {
     return steeringPWM;
 }
 
+// Take in the wanted angular velocity of the handlebar,
+// And return the required duty cycle for that velocity.
 double calculateSteeringPWM(double angularVelocity) {
-    double rpm = -angularVelocity * 30 / pi * gearRatio;  // To comply with Right Hand Rule
-    return 50 + rpm * 40.0 / 1000.0;
+    // Convert from angular velocity (rad/s) of the handlebar,
+    // to rpm of the motor.
+    double rpm = -angularVelocity * 30 / pi * gearRatio;
+
+    // Convert from rpm to duty cycle,
+    // 7787 is the maximum speed of the motor.
+    return 50 + rpm * 40.0 / 7787.0;
 }
 
 double pid(double reference, double currentValue, double Kp, double Ki, double Kd) {
